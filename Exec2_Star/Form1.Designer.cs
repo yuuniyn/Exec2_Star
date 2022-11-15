cec8a354830aade3d5a6a599383e6203c95350b6
Yun ni <erknard2015@gmail.com>
2022-11-16 03:24:48 +0800

    加入專案檔案。

---
+++ Exec2_Star/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Exec2_Star
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblRow = new System.Windows.Forms.Label();
+            this.cmbSelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtShow
+            // 
+            this.txtShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtShow.Location = new System.Drawing.Point(35, 134);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(280, 226);
+            this.txtShow.TabIndex = 4;
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(123, 105);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(75, 23);
+            this.btnshow.TabIndex = 3;
+            this.btnshow.Text = "顯示";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(109, 75);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 2;
+            // 
+            // lblRow
+            // 
+            this.lblRow.AutoSize = true;
+            this.lblRow.Location = new System.Drawing.Point(41, 78);
+            this.lblRow.Name = "lblRow";
+            this.lblRow.Size = new System.Drawing.Size(32, 12);
+            this.lblRow.TabIndex = 1;
+            this.lblRow.Text = "行數:";
+            // 
+            // cmbSelect
+            // 
+            this.cmbSelect.FormattingEnabled = true;
+            this.cmbSelect.Items.AddRange(new object[] {
+            "靠左三角形",
+            "靠右三角形",
+            "等腰三角形"});
+            this.cmbSelect.Location = new System.Drawing.Point(109, 37);
+            this.cmbSelect.Name = "cmbSelect";
+            this.cmbSelect.Size = new System.Drawing.Size(100, 20);
+            this.cmbSelect.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "樣式:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSelect);
+            this.Controls.Add(this.lblRow);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnshow);
+            this.Controls.Add(this.txtShow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtShow;
+		private System.Windows.Forms.Button btnshow;
+		private System.Windows.Forms.TextBox txtInput;
+		private System.Windows.Forms.Label lblRow;
+		private System.Windows.Forms.ComboBox cmbSelect;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Exec2_Star/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Star
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			cmbSelect.Text = "靠左三角形";
+		}
+
+		private void btnshow_Click(object sender, EventArgs e)
+		{
+			int height;
+			try
+			{
+				height = IsTrueValue(txtInput.Text);
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
+
+			if (cmbSelect.Text == "靠左三角形") LeftTriangle(height);
+			if (cmbSelect.Text == "靠右三角形") RightTriangle(height);
+			if (cmbSelect.Text == "等腰三角形") IsoscelesTriangle(height);
+		}
+
+		private int IsTrueValue(string input)
+		{
+			bool isTrue = int.TryParse(input, out int result);
+			if (!isTrue) throw new Exception("輸入值非數字");
+			else if (result < 1 || result > 10) throw new Exception("超過[1, 10]");
+
+			return result;
+		}
+
+		private void LeftTriangle(int height)
+		{
+			string output = string.Empty;
+			for(int i = 1; i <= height; i++)
+			{
+				for(int j = 1; j <= i; j++)
+				{
+					output += "*";
+				}
+				output += "\r\n";
+			}
+			txtShow.Text = output;
+		}
+
+		private void RightTriangle(int height)
+		{
+			string output = string.Empty;
+			for(int i = 1; i <= height; i++)
+			{
+				for(int j = 1; j <= height; j++)
+				{
+					if (j <= height - i ) output += "  ";
+					else output += "*";
+				}
+				output += "\r\n";
+			}
+			txtShow.Text = output;
+		}
+
+		private void IsoscelesTriangle(int height)
+		{
+			string output = string.Empty;
+			for(int i = 1; i <= height; i++)
+			{
+				for(int j = 1; j <= height + i - 1; j++)
+				{
+					if (j < height - i + 1) output += "  ";
+					else output += "*";
+				}
+				output += "\r\n";
+			}
+			txtShow.Text = output;
+		}
+	}
+}
